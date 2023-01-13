ca7062b3a8daf6854ed4d7695f930aa87d505ae0
YerkinSuimenbay <yerkinsuimenbay@gmail.com>
2023-01-13 11:35:02 +0600

    bitrix integration code deleted

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -1,52 +1,20 @@
+import { useState } from "react";
 import { Outlet } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+import ConsultationModal from "../components/rest/modal/ConsultationModal";
+import "./footer.scss";
+
 // import { ReactComponent as LogoFacebook } from "../assets/images/logo-facebook.svg";
 // import { ReactComponent as LogoTwitter } from "../assets/images/logo-twitter.svg";
 import { ReactComponent as LogoInstagram } from "../assets/images/logo-instagram.svg";
 // import { ReactComponent as LogoLinkedin } from "../assets/images/logo-linkedin.svg";
 import logo from "../assets/images/logo-meddata.svg";
-import "./footer.scss";
-import { useTranslation } from "react-i18next";
-import ConsultationModal from "../components/rest/modal/ConsultationModal";
-import { useState } from "react";
 
 const Footer = () => {
   const { t } = useTranslation("common", { keyPrefix: "footer" });
   const [openModal, setOpenModal] = useState(false);
 
-  // const openConsultationModal = () => setOpenModal(true);
-  const openConsultationModal = () => {
-    // setOpenModal(true);
-    
-    (function (w, d, u) {
-      console.log("CLICKED");
-      var s = d.createElement("script");
-      s.async = true;
-      s.src = u + "?" + ((Date.now() / 180000) | 0);
-      var h = d.getElementsByTagName("script")[0];
-      h!.parentNode!.insertBefore(s, h);
-    })(
-      window,
-      document,
-      // "https://cdn-ru.bitrix24.ru/b21027398/c...4.js");
-      "https://cdn-ru.bitrix24.ru/b21027398/crm/form/loader_4.js%27"
-    );
-    // document
-    // <script data-b24-form="inline/4/32qite" data-skip-moving="true">
-    //   (function (w, d, u) {
-    //     var s = d.createElement("script");
-    //     s.async = true;
-    //     s.src = u + "?" + ((Date.now() / 180000) | 0);
-    //     var h = d.getElementsByTagName("script")[0];
-    //     h.parentNode.insertBefore(s, h);
-    //   })(
-    //     window,
-    //     document,
-    //     // "https://cdn-ru.bitrix24.ru/b21027398/c...4.js");
-    //     "https://cdn-ru.bitrix24.ru/b21027398/crm/form/loader_4.js%27"
-    //   );
-    //   // (https://cdn-ru.bitrix24.ru/b21027398/crm/form/loader_4.js%27));
-    // </script>
-  };
+  const openConsultationModal = () => setOpenModal(true);
   const closeConsultationModal = () => setOpenModal(false);
 
   return (
